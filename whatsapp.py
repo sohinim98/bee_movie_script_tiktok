1df61031ae9658225f368297b89991e9942e12e3
Sohini M <smukherjee@Sohinis-MacBook-Pro.local>
2020-05-14 16:29:32 -0400

    master: add the functionality to send the entire bee movie script word by word lol.

--- whatsapp.py
+++ whatsapp.py
@@ -8,6 +8,9 @@ from_whatsapp_number='whatsapp:+14155238886'
 # replace this number with your own WhatsApp Messaging number
 to_whatsapp_number='whatsapp:+12268993426'
 
-client.messages.create(body='noice',
+with open('bee_movie_script.txt','r') as bee_movie:
+    for line in bee_movie:
+        for word in line.split():
+           client.messages.create(body=word,
                        from_=from_whatsapp_number,
                        to=to_whatsapp_number)
